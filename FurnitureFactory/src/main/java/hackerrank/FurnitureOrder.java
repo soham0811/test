87d3f4470a41d48e5a0423b33de917cf001a8b22
soham0811 <92813608+soham0811@users.noreply.github.com>
2021-10-19 22:06:30 +0530

    Update FurnitureOrder.java

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -6,40 +6,51 @@ public class FurnitureOrder implements FurnitureOrderInterface {
     /**
      * TODO: Create a map of Furniture items to order quantities
      */
+    HashMap<Furniture, Integer> hm;
 
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
     FurnitureOrder() {
-        // TODO: Complete the constructor
+        hm = new HashMap<Furniture, Integer>();
     }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
-        // TODO: Complete the method
+        hm.put(type,furnitureCount );
     }
 
     public HashMap<Furniture, Integer> getOrderedFurniture() {
         // TODO: Complete the method
-        return null;
+        return hm;
     }
 
     public float getTotalOrderCost() {
         // TODO: Complete the method
-        return -1.0f;
+      float cost = 0.0;
+        for(Map.Entry m : hm.entrySet()){    
+            cost = cost +m.getValue());    
+           }  
+           return cost;
     }
 
     public int getTypeCount(Furniture type) {
-        // TODO: Complete the method
-        return -1;
+        int count = 0;
+        for(Map.Entry m : hm.entrySet()){    
+            count++;    
+           }  
+           return count;
     }
 
     public float getTypeCost(Furniture type) {
         // TODO: Complete the method
-        return -1.0f;
+        return hm.get(type);
     }
 
     public int getTotalOrderQuantity() {
-        // TODO: Complete the method
-        return -1;
+        int tcount = 0;
+        for(Map.Entry m : hm.entrySet()){    
+            tcount = tcount + m.getValue();
+           }  
+           return tcount;
     }
 }
